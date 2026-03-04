d7650e0e4a7f244e8fe69278d36ead5f6523a21b
Kirubha-123 <kirubhakirubha46@gmail.com>
2026-03-04 18:39:18 +0530

    changes

---
+++ sample.py
@@ -0,0 +1,14 @@
+print("Welcome to Python")
+
+username = "kirubha"
+exp = 10
+is_active = True
+value = 12.000
+
+print("username", username)
+print("exp", exp)
+print(is_active)
+print(type(username))
+print(type(exp))
+print(type(is_active))
+print(type(value))
